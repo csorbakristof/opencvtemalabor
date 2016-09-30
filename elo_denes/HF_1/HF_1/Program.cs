680ea61d4c7e09646ee5e1f40a17cedc64eea016
dinci11 <e.dinci11@gmail.com>
2016-09-30 15:19:05 +0200

    HF_1_DONE

--- elo_denes/HF_1/HF_1/Program.cs
+++ elo_denes/HF_1/HF_1/Program.cs
@@ -10,8 +10,24 @@ namespace HF_1
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
             
+            if (args.Length>1)
+            {
+                string input = args[0].ToString();
+                string output = args[1].ToString();
+
+                Mat image = new Mat(input, ImreadModes.Color);
+                Cv2.Rectangle(image, new Rect(0, 0, image.Cols, image.Rows), new Scalar(0, 255, 255), 3);
+                Cv2.ImWrite(output, image);
+                Cv2.ImShow("new Image", image);
+                Cv2.WaitKey();
+            }
+            else
+            {
+                Console.WriteLine("Give some arguments!");
+                Console.ReadKey();
+            }
 
         }
     }
