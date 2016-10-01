da7b195838b264eed186dc6db6caaa215b5598ec
Szilágyi Norbert <norbi.szilagyi95@gmail.com>
2016-10-01 23:22:29 +0200

    4 heti feladat

---
+++ szilagyinorbi/week_4/Program.cs
@@ -0,0 +1,27 @@
+﻿using OpenCvSharp;
+
+namespace OpenCVTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string filein = args[0];
+            string fileout = args[1];
+
+            Mat picin = new Mat(filein, ImreadModes.Color);
+            Mat picout = new Mat();
+            picin.CopyTo(picout);
+
+            Cv2.Rectangle(picout, new Rect(0, 0, picout.Width, picout.Height), new Scalar(0, 255, 255), 10);
+            Cv2.ImWrite(fileout, picout);
+
+            Cv2.ImShow("image", picin);
+            Cv2.ImShow("framed", picout);
+
+            Cv2.WaitKey();
+
+        }
+    }
+}
