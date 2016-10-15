101f974d9db475233095044a35a601bfbaee2e7f
Kovács Tibor <kovacs.tibor@outlook.hu>
2016-10-15 16:22:14 +0200

    7. heti alapfeladat

---
+++ kovacs_tibor/week7_moving_object_tracking/Program.cs
@@ -0,0 +1,37 @@
+﻿using OpenCvSharp;
+using System;
+
+namespace week7_moving_object_detection
+{
+    class Program
+    {
+        private static string WINDOW_NAME = "Move Detection";
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the path to the video file: ");
+            string inputVideoPath = Console.ReadLine();
+
+            Cv2.NamedWindow(WINDOW_NAME);
+            VideoCapture video = new VideoCapture(inputVideoPath);
+            BackgroundSubtractorMOG2 mog = BackgroundSubtractorMOG2.Create(250, 68, true);
+
+            Mat videoFrame = new Mat();
+            Mat mogFrame = new Mat();
+
+            int inputCharacter = -1;
+            while (video.Read(videoFrame) && inputCharacter != 27)
+            {
+                if (videoFrame.Empty())
+                {
+                    break;
+                }
+
+                mog.Apply(videoFrame, mogFrame);
+
+                Cv2.ImShow(WINDOW_NAME, mogFrame);
+                inputCharacter = Cv2.WaitKey(5);
+            }
+        }
+    }
+}
