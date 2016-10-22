337f65320133a165c287c7670ac42759e69fa645
Szilágyi Norbert <norbi.szilagyi95@gmail.com>
2016-10-22 23:30:28 +0200

    7. heti feladat

---
+++ szilagyinorbi/week7/week7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using OpenCvSharp;
+
+namespace week7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Video file: ");
+            VideoCapture video = new VideoCapture(Console.ReadLine());
+            BackgroundSubtractorMOG2 mog = BackgroundSubtractorMOG2.Create(500, 20, true);
+
+            Mat videoFrame = new Mat();
+            Mat mogFrame = new Mat();
+            Mat dilated = new Mat();
+
+            while (Cv2.WaitKey(30) == -1 && video.Read(videoFrame) && !videoFrame.Empty())
+            {
+                mog.Apply(videoFrame, mogFrame);
+                Cv2.Dilate(mogFrame, dilated, new Mat());
+                Cv2.ImShow("Original", videoFrame);
+                Cv2.ImShow("MOG2", mogFrame);
+                Cv2.ImShow("Dilated", dilated);
+            }
+        }
+    }
+}
+
